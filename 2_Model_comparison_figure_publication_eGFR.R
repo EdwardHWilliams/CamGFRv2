f87319679799ab2231f54e728fe4d994d91ea005
EdwardHywel <edward.williams@cruk.cam.ac.uk>
2020-10-16 00:20:52 +0100

    Finished GFR normal distribution plot, other small edits

--- 2_Model_comparison_figure_publication_eGFR.R
+++ 2_Model_comparison_figure_publication_eGFR.R
@@ -1,7 +1,9 @@
 
+dummy <- data.frame(stat = c("median", "RMSE", "P20 dose"), x = c(0, NA, NA))
 
 p2 <- Results_eGFR_Creattype %>% 
   select(RMSE, median, P20_dose, equation, eGFR_cut, Creatinine_type) %>%
+  rename(`P20 dose` = P20_dose) %>%
   gather(key = stat, value = value, -eGFR_cut, -equation, -Creatinine_type) %>%
   mutate(Creatinine_type = ifelse(Creatinine_type == "IDMS", "IDMS", "Non-IDMS")) %>%
   ggplot(aes(y = value, x = as.numeric(eGFR_cut), colour = equation)) + 
@@ -20,10 +22,8 @@ p2 <- Results_eGFR_Creattype %>%
         strip.text = element_text(size = rel(1))) + 
   facet_grid(stat~Creatinine_type, scales = "free", switch = "y") + 
   ylab("")
+p2
 
-
-ggsave("eGFR_comparison_plot_option2.pdf", plot = p2,
+ggsave("eGFR_comparison_plot.pdf", plot = p2,
        path = fig_export_dir,
        width = 20, height = 20/0.7/1.5, units = "cm")
-   
-
